520076dc92fe46d2ee9aa9a6119fdbcb388861aa
Dyego Aurélio <dyegoaurelio@gmail.com>
2025-03-09 14:20:05 -0300

    adding command to startup gdb ide

--- flake.nix
+++ flake.nix
@@ -3,24 +3,57 @@
   inputs.nixpkgs = {
     url = "github:NixOS/nixpkgs/9855e2d3f265add064386d5db7b66b0d18a9977a";
   };
-  outputs = { self, nixpkgs }: {
+   inputs.nixpkgs-24-11 = {
+    url = "github:NixOS/nixpkgs/16df1b33546abf4d55ee2b895606edd4e0e3612f";
+  };
+  outputs = { self, nixpkgs, nixpkgs-24-11 }: {
 
     defaultPackage.x86_64-linux =
     let
       pkgs =  import nixpkgs { system = "x86_64-linux"; };
+      pkgs-24-11 =  import nixpkgs-24-11 { system = "x86_64-linux"; };
+
+      devPkgs = [
+          pkgs.gdb
+          pkgs-24-11.seer
+      ];
+
+
+      custom-scripts = pkgs.stdenv.mkDerivation {
+
+        # name of our derivation
+        name = "custom-scripts";
+
+        # sources that will be used for our derivation.
+        src = ./scripts;
+
+        # see https://nixos.org/nixpkgs/manual/#ssec-install-phase
+        # $src is defined as the location of our `src` attribute above
+        installPhase = ''
+         # Create a directory at the output path
+          mkdir -p $out/bin
+
+          # Copy scripts into the bin directory and make them executable
+          cp -v $src/* $out/bin/
+          chmod +x $out/bin/*
+        '';
+        buildInputs =  devPkgs ++ [
+        ];
+      };
+
       in
       pkgs.callPackage (import ./firefox-build.nix {
         pkgs = pkgs;
-        mozillaCentralPath = "file:///home/dyego/coding/random/mozilla-central";
+        # mozillaCentralPath = "file:///home/dyego/coding/random/mozilla-central";
         _extraPatches = [
           ./fix-mach-ide-vscode.diff
         ];
         _extraConfigureFlags = [
           "--disable-bootstrap"
         ];
-        _extraBuildInputs = [
+        _extraBuildInputs =  devPkgs ++ [
           pkgs.python312Packages.ninja
-          pkgs.gdb
+          custom-scripts
         ];
       }){};
 
